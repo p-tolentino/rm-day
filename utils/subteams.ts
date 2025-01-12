d3d024da66a21ea73f7c13c55461792a6f864305
Philip Tolentino <philipm.tolentino@gmail.com>
2025-01-12 17:51:46 +0800

    subTeams updated

--- utils/subteams.ts
+++ utils/subteams.ts
@@ -1,13 +1,10 @@
 export const subTeams = [
-  "BLACK OPS",
   "DREAM MAKERS",
   "ELITE",
   "EVEREST",
   "GO GETTERS",
   "HYBRID",
-  "MANGETAR",
   "MAXIMIZE",
   "PM7",
-  "RAINMAKERS",
   "SG",
 ];
