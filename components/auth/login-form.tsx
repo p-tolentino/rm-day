bf4cd999afba3851b9f6a578b8ec71b4e2c714a1
Philip Tolentino <philipm.tolentino@gmail.com>
2025-01-15 16:12:20 +0800

    updated password input, removed dm logo

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -11,6 +11,7 @@ import { useForm } from "react-hook-form";
 import { login } from "@/actions/auth";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { Eye, EyeOff } from "lucide-react";
 
 export function LoginForm({
   className,
@@ -24,8 +25,10 @@ export function LoginForm({
   });
 
   const router = useRouter();
-
+  const [isVisible, setIsVisible] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVisibility = () => setIsVisible((prevState) => !prevState);
 
   const { register, handleSubmit } = useForm<z.infer<typeof authSchema>>({
     resolver: zodResolver(authSchema),
@@ -68,19 +71,42 @@ export function LoginForm({
             placeholder="Wholesaler ID"
             {...register("username")}
             disabled={isLoading}
-            className="border-gray-400"
           />
         </div>
         <div className="grid gap-2">
           <Label htmlFor="password">Password</Label>
-          <Input
-            id="password"
+          <div className="relative">
+            <Input
+              id="password"
+              className="pe-9"
+              placeholder="Password"
+              {...register("password")}
+              type={isVisible ? "text" : "password"}
+              disabled={isLoading}
+            />
+            <button
+              className="absolute inset-y-0 end-0 flex h-full w-9 items-center justify-center rounded-e-lg text-muted-foreground/80 outline-offset-2 transition-colors hover:text-foreground focus:z-10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-ring/70 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+              type="button"
+              onClick={toggleVisibility}
+              aria-label={isVisible ? "Hide password" : "Show password"}
+              aria-pressed={isVisible}
+              aria-controls="password"
+            >
+              {isVisible ? (
+                <EyeOff size={16} strokeWidth={2} aria-hidden="true" />
+              ) : (
+                <Eye size={16} strokeWidth={2} aria-hidden="true" />
+              )}
+            </button>
+          </div>
+          {/* <Input
+            
             type="password"
             placeholder="••••••"
             {...register("password")}
-            disabled={isLoading}
+            
             className="border-gray-400"
-          />
+          /> */}
         </div>
         <Button type="submit" className="w-full" disabled={isLoading}>
           {isLoading ? "Logging In..." : "Login"}
