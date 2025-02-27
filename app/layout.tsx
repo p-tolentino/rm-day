3c219720e127e50f50f83ef29f370fe323380b79
Philip Tolentino <philipm.tolentino@gmail.com>
2025-02-28 00:13:30 +0800

    resume

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,42 @@
 import type { Metadata } from "next";
 import "./globals.css";
-// ! Remove comments
-// import { cookies } from "next/headers";
-// import { createClient } from "@/utils/supabase/server";
-// import { Toaster } from "sonner";
-// import { TooltipProvider } from "@/components/ui/tooltip";
-// import { ImpersonationButton } from "@/components/auth/return-to-admin";
+import { cookies } from "next/headers";
+import { createClient } from "@/utils/supabase/server";
+import { Toaster } from "sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ImpersonationButton } from "@/components/auth/return-to-admin";
 
 export const metadata: Metadata = {
   title: "RM Day Submissions",
 };
 
-export default async function RootLayout({}: // ! Remove comments
-// children,
-Readonly<{
+export default async function RootLayout({
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
-  // ! Remove comments
-  // const supabase = await createClient();
-  // const cookieStore = cookies();
+  const supabase = await createClient();
+  const cookieStore = cookies();
 
-  // // Get the current session
-  // const {
-  //   data: { user },
-  // } = await supabase.auth.getUser();
+  // Get the current session
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  // // Get the admin's email from the cookie
-  // const adminEmail = cookieStore.get("adminEmail")?.value;
+  // Get the admin's email from the cookie
+  const adminEmail = cookieStore.get("adminEmail")?.value;
 
-  // // Check if the current session belongs to an impersonated user
-  // const isImpersonating = adminEmail && user?.email !== adminEmail;
+  // Check if the current session belongs to an impersonated user
+  const isImpersonating = adminEmail && user?.email !== adminEmail;
 
   return (
     <html lang="en">
       <body className="antialiased">
-        {/* <TooltipProvider>
+        <TooltipProvider>
           {children}
           {isImpersonating && <ImpersonationButton />}
           <Toaster richColors />
-        </TooltipProvider> */}
+        </TooltipProvider>
       </body>
     </html>
   );
